9c0000649e390936b66faf32280430ae3c6a649d
Serhii <serhii.kryvopust@globallogic.com>
2020-11-25 00:23:14 +0200

    Add project files.

---
+++ MHAGConverter/Converters.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Text;
+
+namespace MHAGConverter
+{
+    internal class Converters
+    {
+        public bool Ver1(string path)
+        {
+            // Connect to Excel files
+            var myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                                                                     path +
+                                                                     ";Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";");
+            var myCommand = new OleDbDataAdapter("select * from [Sheet1$]", myConnection);
+            var ds = new System.Data.DataSet();
+            myCommand.Fill(ds);
+            myConnection.Close();
+            var rows = ds.Tables[0].Rows;
+            var file =
+                new System.IO.StreamWriter(@"E:\strings.xml",false, Encoding.UTF8);
+            using (file)
+            {
+                const string header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+                file.WriteLine(header);
+                file.WriteLine("<resources>");
+                for (var i = 0; i < rows.Count; i++)
+                {
+                    var row = rows[i];
+                    var items = row.ItemArray;
+                    if (items[1] == null) return false;
+                   
+                    file.WriteLine("<string name=\"{0}\">{1}</string>",
+                        items[0],
+                        ConverterAscii(items));
+                }
+                file.WriteLine("</resources>");
+                return true;
+            }
+
+          
+        }
+        private static string ConverterAscii(IReadOnlyList<object> items) => items[1] == null ? "" : (string) items[1];
+    }
+}
---
+++ MHAGConverter/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MHAGConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "e:\\";
+                openFileDialog.Filter = @"xlsx files (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (var reader = new StreamReader(fileStream))
+                    {
+                        reader.ReadToEnd();
+                    }
+                }
+            }
+            textBox1.Text = filePath;
+
+            //;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($@"Is completed successful: {new Converters().Ver1(textBox1.Text)}",
+                @"STATUS",
+                buttons: MessageBoxButtons.OK);
+        }
+    }
+
+}
